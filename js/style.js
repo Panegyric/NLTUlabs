86a8383c6aa8435dbff91160fc1fada4be837f05
enemycloud <mup7@ukr.net>
2018-03-12 23:07:35 +0200

    Final 10

---
+++ js/style.js
@@ -0,0 +1,21 @@
+function backgroundEct() {
+    document.body.style.background = "#f3f3f3 url(background/white-scratch-lines.png) repeat right top";
+}
+
+function limitEct() {
+    document.getElementById("rowsign").style.border="thick solid #4d90fe";
+}
+
+function listStyleEct() {
+    document.getElementById("ul").style.listStyle = "decimal inside";
+}
+
+function fieldEct() {
+    document.getElementById("field").style.margin="10%";
+    document.getElementById("field").style.padding="5%";
+}
+
+function FontEct() {
+   document.getElementById("p").style.font="italic bold 20px arial,serif";
+    document.getElementById("p").style.color="#4d90fe";
+}
