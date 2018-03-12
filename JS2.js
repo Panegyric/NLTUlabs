fd165b7d60871b6460f5fccb51b48d4bd6cbbaa6
enemycloud <mup7@ukr.net>
2018-03-12 18:19:29 +0200

    new

--- JS2.js
+++
@@ -1,5 +0,0 @@
-function get() {
-    alert("Ім`я = " + document.getElementById('inputName').value + "\n" +
-        "Email = " + document.getElementById('inputEmail').value + "\n" +
-        "Телефон = " + document.getElementById('inputPassword').value);
-}
